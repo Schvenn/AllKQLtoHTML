63b656bb74ca815c42e3b7f7963713a05882e707
Schvenn <42421399+Schvenn@users.noreply.github.com>
2026-04-18 16:40:25 -0400

    v1.5

--- AllKQLtoHTML.psm1
+++ AllKQLtoHTML.psm1
@@ -354,7 +354,7 @@ To acquire your Resource Group and Workspace names, navigate in Sentinel to the 
 If you provide the -merge switch, you should also provide a second JSON file. Without the -merge switch, the second JSON file is ignored.
 
 When merging, the two files can be any combination of an Azure WebShell export or Sentinel UI export, because the script is designed to handle both JSON formats, interchangeably. If you need to merge more than 2 files, it is best that you merge the files of similar JSON format manually first, and then run the script to complete the remaining tasks.
-## Using the concat(enate) switch
+## Using the -concat(enate) switch
 Concatenation in this case is not the same as merge. It is used exclusively for Sentinel UI exports of the ARM formatted JSON files.
 
 When using the Sentinel UI, you will only be able to export a maximum of 50 rules at a time. Using this feature you can combine multiple files into a single ARM JSON file with ease. Simply select all rules, export the contents, navigate to the next page and do the same. Do not change the file name. Let Windows append the usual suffix (1), (2), and so on, until you're done. This script is designed to read those file names and merge them for you, after which it will proceed with the remaining tasks and file generation.
