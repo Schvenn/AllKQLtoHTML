ebe33e0167e12c4552832b5499cd456ec5f7e9c4
Schvenn <42421399+Schvenn@users.noreply.github.com>
2026-05-28 22:45:55 -0400

    v5.4 Improve JSON formatting for column 3 and markdown.

--- AllKQLtoHTML.psm1
+++ AllKQLtoHTML.psm1
@@ -182,8 +182,17 @@ $exclude = @('displayName', 'query', 'description', 'enabled', 'severity', 'temp
 foreach ($p in $Properties.PSObject.Properties) {if ($exclude -contains $p.Name) {continue}
 $key = Escape-Html $p.Name; $val = $p.Value
 
+# ---------------- ALERTDETAILS MAPPINGS ----------------------------------------------------------
+if ($p.Name -eq "alertDetailsOverride") {$lines = foreach ($item in $val.PSObject.Properties) {$k = Escape-Html $item.Name; $v = $item.Value
+if ($k -match 'Severity|ColumnName|Field|Property') {$v = "<span class='ent-col'>$v</span>"}
+if ($null -eq $v) {$v = "{null}"}
+elseif ($v -is [array]) {$v = ($v | ForEach-Object { "$_" }) -join ', '}
+elseif ($v -is [psobject] -and -not ($v -is [string])) {$v = Expand-PropertyValue $v}
+$v = $v -replace '{{([^}]+)}}', "{{<span class='ent-col'>`$1</span>}}"; "$k`: <span class='alert-col'>$v</span>"}
+$valText = $lines -join "`n"}
+
 # ---------------- ENTITY MAPPINGS ----------------------------------------------------------------
-if ($p.Name -eq "entityMappings") {$lines = foreach ($mapping in $val) {$entityType = Escape-Html $mapping.entityType
+elseif ($p.Name -eq "entityMappings") {$lines = foreach ($mapping in $val) {$entityType = Escape-Html $mapping.entityType
 foreach ($field in $mapping.fieldMappings) {$identifier = Escape-Html $field.identifier; $column = Escape-Html $field.columnName; "$entityType`: $identifier`: <span class='ent-col'>$column</span>"}}
 $valText = $lines -join "`n"}
 
